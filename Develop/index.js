3671786ece7ff5d4219b04d96f203986543c4125
Caryn Carter <carter.caryn@gmail.com>
2024-09-21 16:50:26 -0500

    rearranged files

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,52 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: 'What is your location?',
+        name: 'location',
+      },
+      {
+        type: 'input',
+        message: 'What is your bio?',
+        name: 'bio',
+      },
+      {
+        type: 'input',
+        message: 'What is your LinkedIn?',
+        name: 'LinkedIn',
+      },
+    {
+      type: 'input',
+      message: 'What is your Github?',
+      name: 'Github',
+    },
+    {
+      type: 'input',
+      message: 'What is your phone number?',
+      name: 'phone',
+    },
+    {
+      type: 'input',
+      message: 'Where do you want to store the files?',
+      name: 'files',
+    }
+  ])
+  .then((response) => {
+    console.log(response);
+    createHTML(response);
+  }
+  );
+
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
